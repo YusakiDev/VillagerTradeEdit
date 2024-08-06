fe6eb489be3aff56fcba4c63e35d58f940465bfe
_Yusaki <puree.marimjie@gmail.com>
2024-08-06 20:09:43 +0700

    commit

--- src/main/java/org/yusaki/villagertradeedit/YskLibWrapper.java
+++ src/main/java/org/yusaki/villagertradeedit/YskLibWrapper.java
@@ -15,12 +15,37 @@ public class YskLibWrapper {
         this.yskLib = lib;
     }
 
+    void sendActionBar(Player player, String message) {
+        yskLib.sendActionBar(plugin, player, message);
+    }
+
+    void sendTitle(Player player, String title, String subtitle) {
+        yskLib.sendTitle(plugin, player, title, subtitle, 20, 70, 20);
+    }
+
     void sendMessage(Player player, String key, Object... args) {
         yskLib.sendMessage(plugin, player, key, args);
     }
 
+    void updateConfig() {
+        yskLib.updateConfig(plugin);
+    }
+
     public boolean canExecuteInWorld(World world) {
         return yskLib.canExecuteInWorld(plugin, world);
+    }
+
+
+    public void logSevere(String s) {
+        yskLib.logSevere(plugin, s);
+    }
+
+    public void logWarn(String s) {
+        yskLib.logWarn(plugin, s);
+    }
+
+    public void logInfo(String s) {
+        yskLib.logInfo(plugin, s);
     }
 
     public void logDebug(String s) {
@@ -30,5 +55,5 @@ public class YskLibWrapper {
     public void logDebugPlayer(Player player, String s) {
         yskLib.logDebugPlayer(plugin, player, s);
     }
-    
+
 }
